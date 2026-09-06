885c15ccc9b68145914012792f5593d028e6b452
fgeri7 <fgeri7@gmail.com>
2026-09-06 14:19:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,48 +1,14 @@
-const CACHE_NAME = 'splendor-digital-v3.9.2.1-ui-fix';
-const APP_SHELL = ['./', './app.js', './style.css'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  const isShell =
-    url.pathname.endsWith('/app.js') ||
-    url.pathname.endsWith('/style.css') ||
-    url.pathname.endsWith('/');
-
-  if (isShell) {
-    event.respondWith(
-      fetch(event.request).then((response) => {
-        if (response && response.status === 200) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      }).catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+const CACHE="splendor-v3-9-2";
+const ASSETS=[
+  "./","./index.html","./style.css","./app.js","./data.js","./manifest.json",
+  "./favicon.ico",
+  "./icons/icon-48.png","./icons/icon-72.png","./icons/icon-96.png",
+  "./icons/icon-128.png","./icons/icon-144.png","./icons/icon-152.png",
+  "./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png",
+  "./icons/icon-maskable-192.png","./icons/icon-maskable-512.png"
+];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
